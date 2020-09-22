1823aa0b91315bf08a8a5947b5d85e2df06b5408
Tray Lewin <tray.lewin@live.com>
2020-09-22 11:35:09 -0700

    Collapse discover() function Code prettier cleanup

--- packages/eos-transit-algosigner-provider/src/plugin.ts
+++ packages/eos-transit-algosigner-provider/src/plugin.ts
@@ -1,37 +1,47 @@
-import { decodeUint8Array, encodeToUint8Array, discoverAccounts, getWalletAuthForAccount, FIELDS_TO_REMOVE_FROM_TXN} from "./helper"
-import { AlgoNetworkType, SignatureProviderArgs, WalletAuth, NetworkConfig, AlgorandRawTransactionStruct, DiscoverResponse, WalletProvider, SignatureProvider, PushTransactionArgs, ALGOSIGNER_DEFAULT_PERMISSION, DiscoveryOptions, AlgoSignerWalletProviderOptions, TxnObject} from "./types"
-import * as msgpack from "@msgpack/msgpack";
+import {
+  decodeUint8Array,
+  encodeToUint8Array,
+  discoverAccounts,
+  getWalletAuthForAccount,
+  FIELDS_TO_REMOVE_FROM_TXN
+} from './helper';
+import {
+  AlgoNetworkType,
+  SignatureProviderArgs,
+  WalletAuth,
+  NetworkConfig,
+  AlgorandRawTransactionStruct,
+  DiscoverResponse,
+  WalletProvider,
+  SignatureProvider,
+  PushTransactionArgs,
+  ALGOSIGNER_DEFAULT_PERMISSION,
+  DiscoveryOptions,
+  AlgoSignerWalletProviderOptions,
+  TxnObject
+} from './types';
+import * as msgpack from '@msgpack/msgpack';
 import { Buffer } from 'buffer';
-
 
 let _network: AlgoNetworkType = AlgoNetworkType.MainNet;
 let _providedNetwork: AlgoNetworkType | undefined;
 let _loggedInAccount: WalletAuth;
 
-
-
-async function _discover(){
-  return await discoverAccounts(_providedNetwork)
-}
-
-
 export function makeSignatureProvider(): SignatureProvider {
-
   return {
     /** Returns the list of public keys of all the accounts in current network. */
     async getAvailableKeys(): Promise<string[]> {
+      if (_loggedInAccount) return [_loggedInAccount.publicKey];
 
-
-      if(_loggedInAccount)
-        return [_loggedInAccount.publicKey];
-
-      return await (await _discover()).map(account => account.key.key);
-      
+      return (await discoverAccounts(_providedNetwork)).map(
+        account => account.key.key
+      );
     },
 
     /** Signs a transaction using the private keys in AlgoSigner wallet. */
-    async sign({serializedTransaction}: SignatureProviderArgs): Promise<PushTransactionArgs>  {
-      
+    async sign({
+      serializedTransaction
+    }: SignatureProviderArgs): Promise<PushTransactionArgs> {
       const txn = decodeUint8Array<TxnObject>(serializedTransaction);
 
       // Remove fields not needed for chain transaction (AlgoSigner throws if any are present)
@@ -41,15 +51,21 @@ export function makeSignatureProvider(): SignatureProvider {
 
       // This is how algosigner decodes base64 string
 
-      const t = atob(res.blob).split("").map(c => c.charCodeAt(0))
+      const t = atob(res.blob)
+        .split('')
+        .map(c => c.charCodeAt(0));
 
       let arr = new Uint8Array(t);
 
-      const signedTransaction: AlgorandRawTransactionStruct = msgpack.decode(arr) as AlgorandRawTransactionStruct;
+      const signedTransaction: AlgorandRawTransactionStruct = msgpack.decode(
+        arr
+      ) as AlgorandRawTransactionStruct;
 
-      const respone : PushTransactionArgs =  {
-        signatures: [Buffer.from(signedTransaction.sig).toString("hex")], 
-        serializedTransaction: encodeToUint8Array({txn: signedTransaction.txn}),
+      const respone: PushTransactionArgs = {
+        signatures: [Buffer.from(signedTransaction.sig).toString('hex')],
+        serializedTransaction: encodeToUint8Array({
+          txn: signedTransaction.txn
+        })
       };
 
       return respone;
@@ -57,12 +73,11 @@ export function makeSignatureProvider(): SignatureProvider {
   };
 }
 
-
-export function algosignerWalletProvider(props: AlgoSignerWalletProviderOptions) {
-
+export function algosignerWalletProvider(
+  args: AlgoSignerWalletProviderOptions
+) {
   // if network is provided in the constructor, docover should return accounts of this specific network.
   // otherwise discover returns accounts for all networks;
-
   const {
     id = 'algosigner',
     name = 'Algorand AlgoSigner Web Wallet',
@@ -70,27 +85,22 @@ export function algosignerWalletProvider(props: AlgoSignerWalletProviderOptions)
     description = 'Use AlgoSigner Web Wallet to sign your Algorand transactions',
     errorTimeout,
     network
-  } = props;
+  } = args;
 
   _providedNetwork = network;
-  if(network)
-    _network = network;
+  if (network) _network = network;
 
   return function makeWalletProvider(network: NetworkConfig): WalletProvider {
-
-
     /** Verifies that the AlgoSigner plugin exists and password has been entered.  */
     function connect(appName: string): Promise<boolean> {
       return new Promise(async (resolve, reject) => {
-
         AlgoSigner.connect().then(val => {
-          if(val){
+          if (val) {
             resolve(true);
+          } else {
+            reject('AlgoSigner: Connect Error');
           }
-          else{
-            reject("AlgoSigner: Connect Error");
-          }
-        })
+        });
 
         setTimeout(() => {
           reject(`Cannot connect to "${shortName}" wallet provider`);
@@ -103,75 +113,88 @@ export function algosignerWalletProvider(props: AlgoSignerWalletProviderOptions)
       return Promise.resolve(true);
     }
 
-
     /** Returns all accounts in a wallet. If network is provided in the constructor then it only returns accounts for that network.  */
-    async function discover(_discoveryOptions: DiscoveryOptions): Promise<DiscoverResponse[]> {
-      // _discoveryOptions: Remove underscroe from _discoveryOptions when it is actually used. 
+    async function discover(
+      discoveryOptions: DiscoveryOptions
+    ): Promise<DiscoverResponse[]> {
+      // _discoveryOptions: Remove underscroe from _discoveryOptions when it is actually used.
       // it added for now to get away with un-used variable warning.
-      return await _discover();
+      return await discoverAccounts(_providedNetwork);
     }
 
     // Authentication
 
-    function login(accountName?: string, authorization: string = ALGOSIGNER_DEFAULT_PERMISSION): Promise<WalletAuth> {
+    function login(
+      accountName?: string,
+      authorization: string = ALGOSIGNER_DEFAULT_PERMISSION
+    ): Promise<WalletAuth> {
       return new Promise<WalletAuth>(async (resolve, reject) => {
-
-        if(!accountName){
-          throw new Error("AlgoSigner does not support a way for the user to select an account to login. Provide an accountName when calling login().");
+        if (!accountName) {
+          throw new Error(
+            'AlgoSigner does not support a way for the user to select an account to login. Provide an accountName when calling login().'
+          );
         }
- 
+
         let networks: AlgoNetworkType[];
-        if(_network)
-          networks = [_network];
+        if (_network) networks = [_network];
         else
-          networks = [AlgoNetworkType.MainNet, AlgoNetworkType.TestNet, AlgoNetworkType.BetaNet];
-        
-        for(let net of networks){
-          let accounts = await AlgoSigner.accounts({ledger: net});
+          networks = [
+            AlgoNetworkType.MainNet,
+            AlgoNetworkType.TestNet,
+            AlgoNetworkType.BetaNet
+          ];
 
-          let matchingAccout = accounts.find(account => account.address === accountName)
+        for (let net of networks) {
+          let accounts = await AlgoSigner.accounts({ ledger: net });
 
-          if(matchingAccout){
+          let matchingAccout = accounts.find(
+            account => account.address === accountName
+          );
+
+          if (matchingAccout) {
             const loggedInAccount = getWalletAuthForAccount(matchingAccout);
             _loggedInAccount = loggedInAccount;
-            return resolve({permission: authorization, accountName: loggedInAccount.accountName, publicKey: loggedInAccount.publicKey});
+            return resolve({
+              permission: authorization,
+              accountName: loggedInAccount.accountName,
+              publicKey: loggedInAccount.publicKey
+            });
           }
         }
 
-        throw new Error(`Cannot find account with provided address '${accountName}'.`);
-        
+        throw new Error(
+          `Cannot find account with provided address '${accountName}'.`
+        );
       });
     }
-
 
     /** AlgoSigner doesn't store login state hence no action required. */
     function logout(accountName?: string): Promise<boolean> {
       return Promise.resolve(true);
     }
 
-
     /** Algosigner doesnt expose an API to sign arbitrary string. */
     function signArbitrary(data: string, userMessage: string): Promise<string> {
-			return new Promise((resolve, reject) => {
-				reject('not implemented');
-			});
-		}
+      return new Promise((resolve, reject) => {
+        reject('not implemented');
+      });
+    }
 
-		const walletProvider: WalletProvider = {
-			id,
-			meta: {
-				name,
-				shortName,
-				description
-			},
-			signatureProvider: makeSignatureProvider(),
-			connect,
-			discover,
-			disconnect,
-			login,
-			logout,
-      signArbitrary,
-		};
+    const walletProvider: WalletProvider = {
+      id,
+      meta: {
+        name,
+        shortName,
+        description
+      },
+      signatureProvider: makeSignatureProvider(),
+      connect,
+      discover,
+      disconnect,
+      login,
+      logout,
+      signArbitrary
+    };
 
     return walletProvider;
   };
