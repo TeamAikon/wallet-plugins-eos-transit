4913e887a418cda43b240913deb8ae9ac94813a8
Dhruv Kumar Jha <dhruv@dhruvkumarjha.com>
2021-07-07 14:27:54 +0530

    Added the required types for web3 plugin

--- packages/eos-transit-web3-provider/src/types.ts
+++ packages/eos-transit-web3-provider/src/types.ts
@@ -1,52 +1,17 @@
-export interface AlgoSignerType {
+export interface Web3Type {
   connect(): Promise<boolean>;
-  accounts(props: ConnectProps): Promise<AlgoSignerAccountInfo[]>;
-  sign(TxnObject: TxnObject): Promise<AlgoSignerSignSuccessResponse>;
-  send(props: SignProps): Promise<AlgoSignerTransactionSuccessResponse>;
+  accounts(props: ConnectProps): Promise<Web3AccountInfo[]>;
+  sign(TxnObject: TxnObject): Promise<Web3SignSuccessResponse>;
+  send(props: SignProps): Promise<Web3TransactionSuccessResponse>;
 }
-
-declare global {
-  var AlgoSigner: AlgoSignerType;
-}
-
 interface ConnectProps {
   ledger: string;
 }
-
-interface SignProps {
-  tx: string;
-  ledger: string;
-}
-
-export interface AlgoSignerAccountInfo {
-  address: string;
-}
-
-export interface AlgoSignerSignSuccessResponse {
-  txID: string;
-  blob: string;
-}
-
-export interface AlgoSignerTransactionSuccessResponse {
-  txId: string;
-}
-
-type AccountPublicKey = string;
-
-/** stringified account info - account, authorization, network */
-export type DiscoverResponse = {
-  keys: {
-    index: number;
-    key: AccountPublicKey;
-    note: string;
-  }[];
-};
-
 export interface TxnObject {
   to: string;
   from: string;
   fee: number;
-  type: AlgorandTransactionTypeCode;
+  type: EthereumTransactionTypeCode;
   amount: number;
   firstRound: number;
   lastRound: number;
@@ -54,25 +19,12 @@ export interface TxnObject {
   genesisHash: string;
   note: string;
 }
-
-export enum AlgoNetworkType {
-  TestNet = 'TestNet',
-  MainNet = 'MainNet',
-  BetaNet = 'BetaNet'
+interface SignProps {
+  tx: string;
+  ledger: string;
 }
 
-export interface AlgoSignerAccount {
-  account: string;
-  permission: string;
-  publicKey: string;
-}
-
-export interface SignatureProviderArgs {
-  serializedTransaction: Uint8Array;
-  requiredKeys: string[];
-}
-
-export enum AlgorandTransactionTypeCode {
+export enum EthereumTransactionTypeCode {
   AssetConfig = 'acfg',
   AssetFreeze = 'afrz',
   AssetTransfer = 'axfer',
@@ -80,112 +32,51 @@ export enum AlgorandTransactionTypeCode {
   Payment = 'pay'
 }
 
-export type AlgorandTxEncodedForChain = {
-  rcv?: Buffer; // Buffer.from(to.publicKey)
-  name?: string;
-  tag?: Buffer; // Buffer.from(...)
-  amt?: number; // integer
-  note?: Buffer; // Buffer.from(note)
-  snd?: Buffer; // Buffer.from(from.publicKey)
-  type?: AlgorandTransactionTypeCode; // type
-  fv?: number; // firstRound
-  lv?: number; // lastRound
-  fee?: number; // fee
-  gen?: string; // genesisID
-  gn?: string; // genesisHash - Buffer.from(genesisHash, 'base64')
-  lx?: Buffer; // Buffer.from(lease),
-  grp?: Buffer; // group
-  voteKey?: Buffer; // voteKey
-  selkey?: Buffer; // selectionKey
-  votefst?: number; // voteFirst
-  votelst?: number; // voteLast
-  votekd?: number; // voteKeyDilution
-  caid?: number; // assetIndex
-  apar?: {
-    t?: number; // assetTotal
-    df?: boolean; // assetDefaultFrozen
-    dc?: number; // assetDecimals
-    m?: Buffer; // Buffer.from(assetManager.publicKey)
-    r?: Buffer; // Buffer.from(assetReserve.publicKey)
-    f?: Buffer; // Buffer.from(assetFreeze.publicKey)
-    c?: Buffer; // Buffer.from(assetClawback.publicKey)
-    an?: string; // assetName
-    un?: string; // assetUnitName
-    au?: string; // assetURL
-    am?: string; // Buffer.from(assetMetadataHash)
-  };
-  apid?: number; // appIndex,
-  apan?: number; // appOnComplete
-  apls?: {
-    nui?: number; // appLocalInts
-    nbs?: number; // appLocalByteSlices
-  };
-  apgs?: {
-    nui?: number; // appGlobalInts,
-    nbs?: number; // appGlobalByteSlices
-  };
-  apfa?: number[]; // appForeignApps,
-  apap?: Buffer; // Buffer.from(appApprovalProgram)
-  apsu?: Buffer; // Buffer.from(appClearProgram)
-  apaa?: Buffer[]; // appArgs.forEach((arg) => { txn.apaa.push(Buffer.from(arg))
-  apat?: Buffer[]; // appAccounts.forEach((decodedAddress) => { txn.apat.push(Buffer.from(decodedAddress.publicKey))
-  aclose?: Buffer; // Buffer.from(closeRemainderTo.publicKey)
-  asnd?: Buffer; // Buffer.from(assetRevocationTarget.publicKey)
-  fadd?: Buffer; // Buffer.from(freezeAccount.publicKey)
-  afrz?: boolean; // freezeState
-  reKeyTo?: Buffer; // Buffer.from(reKeyTo.publicKey)
-};
-
-export type AlgorandRawTransactionStruct = {
-  txn: AlgorandTxEncodedForChain;
-  sig: Buffer;
-  sgnr?: Buffer;
-};
-
-export interface DiscoveryData {
-  keyToAccountMap: DiscoveryAccount[];
-  keys?: {
-    index: number;
-    key: string;
-  }[];
+export interface Web3AccountInfo {
+  address: string;
 }
-export interface DiscoveryAccount {
-  index: number;
-  key: string;
-  accounts: {
-    account: string;
-    authorization: string;
-  }[];
+export interface Web3SignSuccessResponse {
+  txID: string;
+  blob: string;
+}
+export interface Web3TransactionSuccessResponse {
+  txId: string;
 }
 
-export declare type KeyModifierCallback = (
-  discoveryData: DiscoveryData
-) => DiscoveryData;
-export declare type DiscoverContinueCallback = (
-  discoveredAccounts: DiscoveryAccount[]
-) => void;
-export declare type KeyLookupCallback = (
-  discoveryData: DiscoveryData,
-  callback: DiscoverContinueCallback
-) => void;
+declare global {
+  var Web3: Web3Type;
+}
 
-export interface DiscoveryOptions {
-  pathIndexList: number[];
-  keyModifierFunc?: KeyModifierCallback;
-  keyLookupFunc?: KeyLookupCallback;
-  presetKeyMap?: any;
+export enum EthereumNetworkType {
+  eth_main = 'eth_main',
+  eth_ropsten = 'eth_ropsten',
+}
+
+export interface WalletAuth {
+  accountName: string;
+  permission: string;
+  publicKey?: string;
+}
+
+export interface Web3WalletProviderOptions {
+  id: string;
+  name: string;
+  shortName: string;
+  description?: string;
+  errorTimeout?: number;
+  network?: EthereumNetworkType;
 }
 
 export interface WalletProviderMetadata {
   name?: string;
   shortName?: string;
   description?: string;
-}
-
-export interface WalletAuth {
-  accountName: string;
-  permission: string;
-  publicKey: string;
+  isWalletInterface?: boolean;
+  walletMetadata?: {
+    name?: string;
+    shortName?: string;
+    description?: string;
+  }
 }
 
 export interface WalletProvider {
@@ -205,11 +96,6 @@ export interface WalletProvider {
   signArbitrary(data: string, userMessage: string): Promise<string>;
 }
 
-export interface PushTransactionArgs {
-  signatures: string[];
-  serializedTransaction: Uint8Array;
-}
-
 export interface SignatureProvider {
   /** Public keys associated with the private keys that the `SignatureProvider` holds */
   getAvailableKeys: () => Promise<string[]>;
@@ -217,9 +103,66 @@ export interface SignatureProvider {
   sign: (args: SignatureProviderArgs) => Promise<PushTransactionArgs>;
 }
 
+export interface DiscoveryOptions {
+  pathIndexList: number[];
+  keyModifierFunc?: KeyModifierCallback;
+  keyLookupFunc?: KeyLookupCallback;
+  presetKeyMap?: any;
+}
+
+/** stringified account info - account, authorization, network */
+export type DiscoverResponse = {
+  keys: {
+    index: number;
+    key: AccountPublicKey;
+    note: string;
+  }[];
+};
+
+type AccountPublicKey = string;
+
+export interface SignatureProviderArgs {
+  serializedTransaction: Uint8Array;
+  requiredKeys: string[];
+}
+
 export interface PushTransactionArgs {
   signatures: string[];
   serializedTransaction: Uint8Array;
+}
+
+export declare type KeyModifierCallback = (
+  discoveryData: DiscoveryData
+) => DiscoveryData;
+
+export declare type KeyLookupCallback = (
+  discoveryData: DiscoveryData,
+  callback: DiscoverContinueCallback
+) => void;
+
+export interface DiscoveryData {
+  keyToAccountMap: DiscoveryAccount[];
+  keys?: {
+    index: number;
+    key: string;
+  }[];
+}
+export declare type DiscoverContinueCallback = (
+  discoveredAccounts: DiscoveryAccount[]
+) => void;
+
+export interface DiscoveryAccount {
+  index: number;
+  key: string;
+  accounts: {
+    account: string;
+    authorization: string;
+  }[];
+}
+
+export interface SignatureProvider {
+  getAvailableKeys: () => Promise<string[]>;
+  sign: (args: SignatureProviderArgs) => Promise<PushTransactionArgs>;
 }
 
 export interface NetworkConfig {
@@ -230,11 +173,5 @@ export interface NetworkConfig {
   chainId: string;
 }
 
-export interface AlgoSignerWalletProviderOptions {
-  id: string;
-  name: string;
-  shortName: string;
-  description?: string;
-  errorTimeout?: number;
-  network?: AlgoNetworkType;
-}
+
+
