9634dbbec52ad878607d4697ffc8b700d620af14
Dhruv Kumar Jha <dhruv@dhruvkumarjha.com>
2021-07-07 17:22:39 +0530

    Added web3 plugin and its methods

--- packages/eos-transit-web3-provider/src/plugin.ts
+++ packages/eos-transit-web3-provider/src/plugin.ts
@@ -1,181 +1,200 @@
+import { ethers } from 'ethers';
+import { decode } from '@msgpack/msgpack';
 import {
-  decodeUint8Array,
-  encodeToUint8Array,
-  discoverAccounts,
-  getWalletAuthForAccount,
-  FIELDS_TO_REMOVE_FROM_TXN,
-  ALL_ALGORAND_NETWORKS,
-  ALGOSIGNER_DEFAULT_PERMISSION,
-} from './helper';
-import {
-  AlgoNetworkType,
-  SignatureProviderArgs,
   WalletAuth,
-  NetworkConfig,
-  AlgorandRawTransactionStruct,
-  DiscoverResponse,
+  Web3WalletProviderOptions,
   WalletProvider,
-  SignatureProvider,
-  PushTransactionArgs,
   DiscoveryOptions,
-  AlgoSignerWalletProviderOptions,
-  TxnObject,
+  DiscoverResponse,
+  SignatureProvider,
+  SignatureProviderArgs,
+  PushTransactionArgs,
+  NetworkConfig,
 } from './types';
-// NOTE: @msgpack/msgpack/dist is required due to packaging error in msgpack that causes build errors (for mjs files) in other projects 
-// See: https://github.com/msgpack/msgpack-javascript/issues/169
-import * as msgpack from '@msgpack/msgpack/dist';
-import { Buffer } from 'buffer';
+import {
+  WEB3_DEFAULT_PERMISSION,
+} from './helper'
 
-let _network: AlgoNetworkType = AlgoNetworkType.MainNet;
-let _providedNetwork: AlgoNetworkType | undefined;
-let _loggedInAccount: WalletAuth;
+let provider: any
+let signer: any
+
+declare const window: any
 
 export function makeSignatureProvider(): SignatureProvider {
   return {
-    /** Returns the list of public keys of all the accounts in current network. */
     async getAvailableKeys(): Promise<string[]> {
-      if (_loggedInAccount) return [_loggedInAccount.publicKey];
-      return (await discoverAccounts(_providedNetwork)).keys.map(
-        account => account.key // AlgoSigner has only one key per account
-      );
-    },
-
-    /** Signs a transaction using the private keys in AlgoSigner wallet. */
-    async sign({
-      serializedTransaction
-    }: SignatureProviderArgs): Promise<PushTransactionArgs> {
-      const txn = decodeUint8Array<TxnObject>(serializedTransaction);
-
-      // Remove fields not needed for chain transaction (AlgoSigner throws if any are present)
-      FIELDS_TO_REMOVE_FROM_TXN.map(field => delete txn[field]);
-
-      const res = await AlgoSigner.sign(txn);
-
-      // This is how algosigner decodes base64 string
-
-      const t = atob(res.blob)
-        .split('')
-        .map(c => c.charCodeAt(0));
-
-      let arr = new Uint8Array(t);
-
-      const signedTransaction: AlgorandRawTransactionStruct = msgpack.decode(
-        arr
-      ) as AlgorandRawTransactionStruct;
-
-      const respone: PushTransactionArgs = {
-        signatures: [Buffer.from(signedTransaction.sig).toString('hex')],
-        serializedTransaction: encodeToUint8Array({
-          txn: signedTransaction.txn
-        })
-      };
-
-      return respone;
-    }
-  };
-}
-
-export function algosignerWalletProvider(
-  args: AlgoSignerWalletProviderOptions
-) {
-  // if network is provided in the constructor, docover should return accounts of this specific network.
-  // otherwise discover returns accounts for all networks;
-  const {
-    id = 'algosigner',
-    name = 'Algorand AlgoSigner Web Wallet',
-    shortName = 'AlgoSigner',
-    description = 'Use AlgoSigner Web Wallet to sign your Algorand transactions',
-    errorTimeout,
-    network
-  } = args || {};
-
-  _providedNetwork = network;
-  if (network) _network = network;
-
-  return function makeWalletProvider(network: NetworkConfig): WalletProvider {
-    /** Verifies that the AlgoSigner plugin exists and password has been entered.  */
-    function connect(appName: string): Promise<boolean> {
-      return new Promise(async (resolve, reject) => {
-        AlgoSigner.connect().then(val => {
-          if (val) {
-            resolve(true);
-          } else {
-            reject('AlgoSigner: Connect Error');
-          }
-        });
-
-        setTimeout(() => {
-          reject(`Cannot connect to "${shortName}" wallet provider`);
-        }, errorTimeout || 2500);
-      });
-    }
-
-    /** AlgoSigner doesn't store connection state hence no action required. */
-    function disconnect(): Promise<boolean> {
-      return Promise.resolve(true);
-    }
-
-    /** Returns all accounts in a wallet. If network is provided in the constructor then it only returns accounts for that network.  */
-    async function discover(
-      discoveryOptions: DiscoveryOptions
-    ): Promise<DiscoverResponse> {
-      // _discoveryOptions: Remove underscroe from _discoveryOptions when it is actually used.
-      // it added for now to get away with un-used variable warning.
-      return await discoverAccounts(_providedNetwork);
-    }
-
-    // Authentication
-
-    function login(
-      accountName?: string,
-      authorization: string = ALGOSIGNER_DEFAULT_PERMISSION
-    ): Promise<WalletAuth> {
-      return new Promise<WalletAuth>(async (resolve, reject) => {
-        if (!accountName) {
-          throw new Error(
-            'AlgoSigner does not support a way for the user to select an account to login. Provide an accountName when calling login().'
-          );
-        }
-
-        let networks: AlgoNetworkType[];
-        if (_network) networks = [_network];
-        else
-          networks = ALL_ALGORAND_NETWORKS
-
-        for (let net of networks) {
-          let accounts = await AlgoSigner.accounts({ ledger: net });
-
-          let matchingAccout = accounts.find(
-            account => account.address === accountName
-          );
-
-          if (matchingAccout) {
-            const loggedInAccount = getWalletAuthForAccount(matchingAccout);
-            _loggedInAccount = loggedInAccount;
-            return resolve({
-              permission: authorization,
-              accountName: loggedInAccount.accountName,
-              publicKey: loggedInAccount.publicKey
-            });
-          }
-        }
-
-        throw new Error(
-          `Cannot find account with provided address '${accountName}'.`
-        );
-      });
-    }
-
-    /** AlgoSigner doesn't store login state hence no action required. */
-    function logout(accountName?: string): Promise<boolean> {
-      return Promise.resolve(true);
-    }
-
-    /** Algosigner doesnt expose an API to sign arbitrary string. */
-    function signArbitrary(data: string, userMessage: string): Promise<string> {
       return new Promise((resolve, reject) => {
         reject('not implemented');
       });
+    },
+
+    async sign({
+      serializedTransaction
+    }: SignatureProviderArgs): Promise<PushTransactionArgs> {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const decoded: any = decode(serializedTransaction)
+          console.log('decoded', decoded)
+  
+          if ( decoded.contract ) {
+            const { contract: decodedContract } = decoded
+
+            const address = await signer.getAddress()
+            const contract = new ethers.Contract(address, decodedContract.abi, provider)
+            const connected = contract.connect(signer)
+            const transaction = await connected.functions[decodedContract.method](...decodedContract.parameters)
+            // let data = await contract.staticCall.transfer(toAddress, amount);
+
+            resolve({
+              signatures: transaction,
+              serializedTransaction,
+            })
+          } else {
+            let finalTransaction = { ...decoded }
+            if ( decoded?.value && typeof decoded.value === 'string' ) {
+              finalTransaction = {
+                ...decoded,
+                value: ethers.utils.parseEther(decoded.value),
+              }
+            }
+            const sendTransaction = await signer.sendTransaction(finalTransaction)
+            resolve({
+              signatures: sendTransaction,
+              serializedTransaction,
+            })
+
+          }
+        } catch (error) {
+          reject(error)
+        }
+
+      })
+    },
+  }
+}
+
+
+
+export function web3WalletProvider(
+  args: Web3WalletProviderOptions
+) {
+
+  const {
+    id = 'web3',
+    name = 'Web3 Web Wallet',
+    shortName = 'Web3',
+    description = 'Use Web3 Web Wallet to sign your Ethereum transactions',
+    errorTimeout,
+    network
+  } = args || {}
+
+  return function makeWalletProvider(network: NetworkConfig): WalletProvider {
+    console.log('web3WalletProvider:makeWalletProvider :: network', network)
+
+    /** Verifies that the Web3 extension exists and password has been entered.  */
+    function connect(appName: string): Promise<boolean> {
+      return new Promise(async (resolve, reject) => {
+        console.log('web3WalletProvider::web3WalletProvider::connect::appName', appName)
+        console.log('network', network)
+        // A Web3Provider wraps a standard Web3 provider, which is
+        // what Metamask injects as window.ethereum into each page
+        try {
+          // provider = await new ethers.providers.Web3Provider(window.ethereum, 'any')
+          // TODO: Remove the code below and it might be deprecated in future
+          await window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum, 'any'))
+
+          console.log('web3WalletProvider::web3WalletProvider::provider', provider)
+          const accounts = await provider.send('eth_requestAccounts')
+          console.log('web3WalletProvider::web3WalletProvider::accounts', accounts)
+          signer = provider.getSigner()
+          console.log('signer', signer)
+
+          const network = await provider.getNetwork()
+          console.log('network', network)
+          const chainId = network.chainId
+          console.log('chainId', chainId)
+
+
+          // setup chainChanged listener
+          // TODO: Update the types
+          provider.on('network', (network: any, oldNetwork: any) => {
+            console.log('CONNECT::netowrk changed event')
+            console.log('network::new', network)
+            console.log('network::oldNetwork', oldNetwork)
+            console.log('network.chainId', network.chainId)
+          })
+        
+          // setup accountsChanged listener
+          // https://github.com/ethers-io/ethers.js/issues/1396
+          // https://github.com/ethers-io/ethers.js/discussions/1560
+          window.ethereum.on('accountsChanged', (accounts: any) => {
+            console.log('CONNECT::accountsChanged changed event')
+            console.log('accountsChanged::accounts', accounts)            
+          })
+          
+
+          if (accounts.length > 0) {
+            resolve(true)
+          } else {
+            reject('Web3: Error Getting Accounts')
+          }
+        } catch (error) {
+          console.log('error', error)
+          reject('Web3: Connect Error')
+        }
+      })
+    }
+
+
+    /**
+     * This method is currently not supported as web3 wallets doesn't support this functionality
+     * This method should list all the accounts user has in their wallet
+     */
+    async function discover(discoveryOptions: DiscoveryOptions): Promise<DiscoverResponse> {
+      return new Promise(async (resolve, reject) => {
+        reject('Web3: Discover is not supported')
+      })
+    }
+
+
+    function disconnect(): Promise<boolean> {
+      console.log('web3WalletProvider::disconnect')
+      return Promise.resolve(true);
+    }
+    
+
+    function login(accountName: string, authorization: string = WEB3_DEFAULT_PERMISSION) : Promise<WalletAuth> {
+      return new Promise<WalletAuth>(async (resolve, reject) => {
+        try {
+          const accounts = await provider.send('eth_requestAccounts')
+          const account = accounts[0]
+          const output = {
+            permission: 'active',
+            accountName: account,
+          }
+          return resolve(output)  
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+
+    function logout(accountName?: string): Promise<boolean> {
+      return new Promise((resolve, reject) => {
+        resolve(true);
+      });
+    }
+
+    function signArbitrary(data: string, userMessage: string): Promise<any> {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const signedTransaction = await signer.signMessage(data)
+          const splitSignature = ethers.utils.splitSignature(signedTransaction)
+          resolve(signedTransaction)
+        } catch (err) {
+          reject(err) 
+        }
+      })
     }
 
     const walletProvider: WalletProvider = {
@@ -192,10 +211,12 @@ export function algosignerWalletProvider(
       login,
       logout,
       signArbitrary
-    };
+    }
 
     return walletProvider;
-  };
+
+  }
+
 }
 
-export default algosignerWalletProvider;
+export default web3WalletProvider;
