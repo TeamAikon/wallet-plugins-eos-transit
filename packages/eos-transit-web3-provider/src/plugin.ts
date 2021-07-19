afd07d8a33ded98cbe1b7d90ca6e2ec20b4295d4
Dhruv Kumar Jha <dhruv@dhruvkumarjha.com>
2021-07-19 20:08:07 +0530

    Updated the plugin class with required methods and types

--- packages/eos-transit-web3-provider/src/plugin.ts
+++ packages/eos-transit-web3-provider/src/plugin.ts
@@ -1,38 +1,99 @@
-import { ethers, providers, Signer } from 'ethers';
-import { decode, encode } from '@msgpack/msgpack';
-import {
-  DiscoveryAccount,
-  DiscoverResponse,
-  DiscoveryOptions,
-  NetworkConfig,
-  PushTransactionArgs,
-  SignatureProvider,
-  SignatureProviderArgs,
-  Web3WalletProviderOptions,
-  WalletAuth,
-  WalletProvider,
-  WalletProviderMetadata,
-} from './types';
-import Web3 from './Web3';
-import { isAString, WEB3_DEFAULT_PERMISSION } from './helper';
+import { providers } from 'ethers';
+import Web3, { WalletProviderMetadata } from './Web3';
 
+/** Check if the given value is a string or instance of string */
+export function isAString(value: any): boolean {
+  if (!value) return false
+  return typeof value === 'string' || value instanceof String
+}
 
 declare const window: any;
 
 class Web3ProviderPlugin extends Web3 {
 
-  constructor(props: any) {
-    super(props);
+  constructor(metaData: WalletProviderMetadata) {
+    super(metaData);
+
+    this.assertIsDesiredNetwork = this.assertIsDesiredNetwork.bind(this);
+    this.getChainIdFromNetwork = this.getChainIdFromNetwork.bind(this);
+    this.handleOnSelectAccount = this.handleOnSelectAccount.bind(this);
+    this.handleOnSelectNetwork = this.handleOnSelectNetwork.bind(this);
+    this.popupSelectDesiredNetworkIfNeeded = this.popupSelectDesiredNetworkIfNeeded.bind(this);
   }
 
   async connect(appName: string): Promise<boolean> {
-    await window?.ethereum?.enable();
-    return super.connect(appName, window.ethereum);
+    return new Promise(async (resolve, reject) => {
+      try {
+        await window?.ethereum?.enable();
+        const res = await super.connect(appName, window.ethereum);
+
+        // confirm current selected network matches requested network
+        await this.popupSelectDesiredNetworkIfNeeded(this.networkConfig);
+        resolve(res);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  setupEventListeners() {
+    // setup network change listener
+    this.provider.on('network', this.handleOnSelectNetwork);
+
+    // setup account change listener
+    window?.ethereum?.on('accountsChanged', this.handleOnSelectAccount);
+  }
+
+  /** Handle network change event */
+  handleOnSelectNetwork(network: providers.Network, oldNetwork: providers.Network): void {
+    this.selectedNetwork = network;
+    this.discover();
+  }
+
+  /** Handle account change event */
+  handleOnSelectAccount(accounts: string[]) {
+    const account = accounts?.length > 0 ? accounts[0] : undefined;
+    this.selectedAccount = account;
+    this.discover();
+  }
+
+  /** if requiredNetwork is not already selected in the wallet, trigger the wallet to prmompt the user to select the network */
+  async popupSelectDesiredNetworkIfNeeded(requiredNetwork: any & { chainId: number | string }): Promise<void> {
+    const { chainIdInt, chainIdHex } = this.getChainIdFromNetwork(requiredNetwork);
+    if (this.selectedNetwork?.chainId === chainIdInt) return;
+    // propmt the user to select the correct network
+    await window?.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: chainIdHex }]
+    });
+    this.selectedNetwork = await this.provider.getNetwork();
+    // if desired network not selected, throw
+    this.assertIsDesiredNetwork(requiredNetwork);
+  }
+
+  /** convert network chainId to int if needed */
+  getChainIdFromNetwork(network: any & { chainId: number | string }): { chainIdInt: number; chainIdHex: string } {
+    const { chainId } = network;
+    const chainIdInt = isAString(chainId) ? parseInt(chainId) : chainId;
+    const chainIdHex = `0x${chainIdInt}`;
+    return { chainIdInt, chainIdHex };
+  }
+
+  /** reject if requiredNetwork is not already selected in the wallet */
+  assertIsDesiredNetwork(requiredNetwork: any & { chainId: number | string }): void {
+    const { chainIdInt } = this.getChainIdFromNetwork(requiredNetwork);
+    if (this.selectedNetwork?.chainId !== chainIdInt) {
+      const errMsg = `Desired network not selected in wallet: Please select the it using the Wallet. Specified Network: ${JSON.stringify(
+        requiredNetwork
+      )}`;
+      throw new Error(errMsg);
+    }
   }
 
 }
 
-const config: WalletProviderMetadata = {
+// plugin meta data
+const metaData: WalletProviderMetadata = {
   id: 'web3',
   name: 'Web3 Web Wallet',
   shortName: 'Web3',
@@ -47,7 +108,7 @@ const config: WalletProviderMetadata = {
 
 
 const web3ProviderPlugin = () => {
-  const plugin = new Web3ProviderPlugin(config);
+  const plugin = new Web3ProviderPlugin(metaData);
   // return the wallet provider
   return plugin.makeWalletProvider;
 };
