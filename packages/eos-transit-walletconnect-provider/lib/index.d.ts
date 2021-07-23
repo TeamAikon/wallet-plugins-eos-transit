49e2ff48958386e90ae3fbcd3fd1c00796a733f2
Dhruv Kumar Jha <dhruv@dhruvkumarjha.com>
2021-07-23 14:19:40 +0530

    Removed lib directory from git

--- packages/eos-transit-walletconnect-provider/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import walletConnectProvider from './plugin';
-export default walletConnectProvider;
-export { EthereumNetworkType } from './EosTransitWeb3ProviderCore';
--- packages/eos-transit-walletconnect-provider/lib/plugin.d.ts
+++
@@ -1,3 +0,0 @@
-import { Web3WalletProviderOptions } from './EosTransitWeb3ProviderCore';
-declare const walletConnectProviderPlugin: (args: Web3WalletProviderOptions) => (network: import("./EosTransitWeb3ProviderCore").NetworkConfig) => import("./EosTransitWeb3ProviderCore").WalletProvider;
-export default walletConnectProviderPlugin;
